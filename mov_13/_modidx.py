d4c0cde1f4782ad86fe7adbfee8bef1b32a734ca
daveramseymusic <63751809+daveramseymusic@users.noreply.github.com>
2022-10-12 14:37:24 -0400

    run notebook and make sure it works then commit

--- mov_13/_modidx.py
+++ mov_13/_modidx.py
@@ -5,4 +5,8 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://daveramseymusic.github.io',
                 'git_url': 'https://github.com/daveramseymusic/mov-13',
                 'lib_path': 'mov_13'},
-  'syms': {'mov_13.core': {'mov_13.core.foo': ('core.html#foo', 'mov_13/core.py')}}}
+  'syms': { 'mov_13.core': {'mov_13.core.foo': ('core.html#foo', 'mov_13/core.py')},
+            'mov_13.movie_recommender': { 'mov_13.movie_recommender.get_movie_recs': ( 'movie_recommender.html#get_movie_recs',
+                                                                                       'mov_13/movie_recommender.py'),
+                                          'mov_13.movie_recommender.search_movies': ( 'movie_recommender.html#search_movies',
+                                                                                      'mov_13/movie_recommender.py')}}}
