4bc0b83fe65887df2b0921310b035f1a0f99ec85
daveramseymusic <63751809+daveramseymusic@users.noreply.github.com>
2022-10-12 16:38:26 -0400

    delete core file

--- mov_13/_modidx.py
+++ mov_13/_modidx.py
@@ -5,8 +5,7 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://daveramseymusic.github.io',
                 'git_url': 'https://github.com/daveramseymusic/mov-13',
                 'lib_path': 'mov_13'},
-  'syms': { 'mov_13.core': {'mov_13.core.foo': ('core.html#foo', 'mov_13/core.py')},
-            'mov_13.movie_recommender': { 'mov_13.movie_recommender.get_movie_recs': ( 'movie_recommender.html#get_movie_recs',
+  'syms': { 'mov_13.movie_recommender': { 'mov_13.movie_recommender.get_movie_recs': ( 'movie_recommender.html#get_movie_recs',
                                                                                        'mov_13/movie_recommender.py'),
-                                          'mov_13.movie_recommender.search_movies': ( 'movie_recommender.html#search_movies',
-                                                                                      'mov_13/movie_recommender.py')}}}
+                                          'mov_13.movie_recommender.search_movies_n_recommend': ( 'movie_recommender.html#search_movies_n_recommend',
+                                                                                                  'mov_13/movie_recommender.py')}}}
